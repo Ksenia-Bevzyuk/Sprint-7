2ff0a2a20296c3818de13e2f033a16840071d2f6
Ksenia Bevzyuk <ksunechka-92@yandex.ru>
2025-02-09 02:22:09 +0300

    modified ListOfOrders class

--- src/main/java/model/ListOfOrders.java
+++ src/main/java/model/ListOfOrders.java
@@ -1,14 +1,9 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import io.qameta.allure.restassured.AllureRestAssured;
-import io.restassured.response.ValidatableResponse;
-
-import static io.restassured.RestAssured.given;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ListOfOrders {
-    private String baseURI;
 
     private String id;
     private String courierId;
@@ -16,14 +11,6 @@ public class ListOfOrders {
     public ListOfOrders(String id, String courierId) {
         this.id = id;
         this.courierId = courierId;
-    }
-
-    public ListOfOrders(String baseURI) {
-        this.baseURI = baseURI;
-    }
-
-    public String getBaseURI() {
-        return baseURI;
     }
 
     public ListOfOrders() {
@@ -43,78 +30,5 @@ public class ListOfOrders {
 
     public void setCourierId(String courierId) {
         this.courierId = courierId;
-    }
-
-    public ValidatableResponse getOrdersWithLimit() {
-        return given()
-                .filter(new AllureRestAssured())
-                .log()
-                .all()
-                .baseUri(baseURI)
-                .header("Content-Type", "application/json")
-                .queryParam("limit", "2")
-                .get("/api/v1/orders")
-                .then()
-                .log()
-                .all();
-    }
-
-    public ValidatableResponse getOrdersWithPage() {
-        return given()
-                .filter(new AllureRestAssured())
-                .log()
-                .all()
-                .baseUri(baseURI)
-                .header("Content-Type", "application/json")
-                .queryParam("page", "1")
-                .get("/api/v1/orders")
-                .then()
-                .log()
-                .all();
-    }
-
-    public ValidatableResponse getOrdersWithStation() {
-        return given()
-                .filter(new AllureRestAssured())
-                .log()
-                .all()
-                .baseUri(baseURI)
-                .header("Content-Type", "application/json")
-                .queryParam("nearestStation", "[\"1\"]")
-                .get("/api/v1/orders")
-                .then()
-                .log()
-                .all();
-    }
-
-    public ValidatableResponse getOrdersWithLimitAndPage() {
-        return given()
-                .filter(new AllureRestAssured())
-                .log()
-                .all()
-                .baseUri(baseURI)
-                .header("Content-Type", "application/json")
-                .queryParam("limit", "2")
-                .queryParam("page", "1")
-                .get("/api/v1/orders")
-                .then()
-                .log()
-                .all();
-    }
-
-    public ValidatableResponse getOrdersWithLimitAndPageAndStation() {
-        return given()
-                .filter(new AllureRestAssured())
-                .log()
-                .all()
-                .baseUri(baseURI)
-                .header("Content-Type", "application/json")
-                .queryParam("limit", "2")
-                .queryParam("page", "1")
-                .queryParam("nearestStation", "[\"1\"]")
-                .get("/api/v1/orders")
-                .then()
-                .log()
-                .all();
     }
 }
